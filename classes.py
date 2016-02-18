6fd0a01145fe750466d1f7fb28775e8ee62ae2c5
camm0991 <carlos.mtz0991@gmail.com>
2016-02-18 00:28:35 -0600

    Redefined class paramaeters and diffMatrix function

--- classes.py
+++ classes.py
@@ -1,63 +1,47 @@
 import numpy as np
 import pandas as pd
 
-def diffmatrix(X, comp_functions):
-    #Obtain the standart deviation for each column except the last (class)
-    X_stdvect = X.ix[:,:-1].apply(comp_functions[0], 0)
-    #Get number of rows and columns
-    (numr, labels) = X.shape
+"""
+X: The Pandas data set.
+comp_array: The array of values to compare two rows.
+comp_function: A lambda function for comparing for determining how to compare the values.
+"""
+
+def diffmatrix(X, comp_array, comp_function):
+    #Get number of rows
+    num_rows = len(X)
     #Obtain classes and the index of the first appearance
     classes = []
-    classes_index = []
-    comparator = lambda x,y,z: 1 if x + z > y and x - z < y else 0
-    
+    classes_index = []    
     for i in X.itertuples():
         if i[-1] not in classes:
             classes.append(i[-1])
             classes_index.append(i[0])
   
-    
+    #From the first to the n-1 class
     for i in xrange(classes[-1]-1):
+        #Starting at the first appearance of the class i and ends on the class i+1 first index
         for j in xrange(classes_index[i], classes_index[i+1]):
-            for k in xrange(classes_index[i+1], numr):
-               print(','.join(map(str, map(comparator,X.loc[j][:-1],X.loc[k][:-1],X_stdvect))))
-    
-    
-    #for i in xrange(numr-1):
-    #    for j in xrange(i+1, numr):
-    #        if X.loc[i][-1] != X.loc[j][-1]:
-    #            print(str(i) + ',' + str(j))
-    #            print(','.join(map(str, map(comparator,X.loc[i][:-1],X.loc[j][:-1],X_stdvect))))
-    #            
-#def basicmatrix():
+            #From the end of the i class to the end of the data
+            for k in xrange(classes_index[i+1], num_rows):
+               #Compares based on a lambda the j-th row with the other k rows
+               print(','.join(map(str, map(comp_function,X.loc[j][:-1],X.loc[k][:-1],comp_array))))
 
-
-def compare(row1, row2, tolerance):
-    new_row = row1
-    
-    
-
+#Calculate the testors making all the comparations with the matrix rows
 class BruteForce(object):
-    def __init__(self, X, comp_functions=[np.std]):
+    def __init__(self, X, comp_array, comp_function):
         self.X = X
-        if len(comp_functions) == 1 or len(comp_functions) == len(X.columns) - 1 :
-            self.comp_functions = comp_functions
-        else :
-            print('Functions must be one per attribute or only one')
-            return
+        self.comp_array = comp_array
+        self.comp_function = comp_function
     
-    #Returns an array of testors
+    #Returns an array of typycal testors
     def calculate_testors(self):
-        dmatrix = diffmatrix(self.X, self.comp_functions)
-        #bmatrix = basicmatrix(dmatrix)
+        dmatrix = diffmatrix(self.X, self.comp_array, self.comp_function)
 
 class CT(object):
 
-    def __init__(self, X, comp_functions):
-        if (len(comp_functions) == 1) or (len(comp_functions) == len(X.columns) - 1) :
-            self.X = X
-            self.comp_functions = comp_functions
-        else :
-            print('Functions must be one per attribute or only one')
-            return
-        print(len(X.columns))
+    def __init__(self, X, comp_array, comp_function):
+        self.X = X
+        self.comp_array = comp_array
+        self.comp_function = comp_function
+        print("Not implemented yet")
